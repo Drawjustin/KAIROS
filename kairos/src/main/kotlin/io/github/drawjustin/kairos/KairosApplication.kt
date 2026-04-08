1c999f279862ece588d7193bdd1f92dcc67d301b
justin <justin@justinui-MacBookAir.local>
2026-04-08 23:21:22 +0900

    refactor: UserDetailsServiceAutoConfiguration waring message Spring Security 기본 warning message 계정 생성 및 안내에 관한 Configuration 클래스 제외

--- kairos/src/main/kotlin/io/github/drawjustin/kairos/KairosApplication.kt
+++ kairos/src/main/kotlin/io/github/drawjustin/kairos/KairosApplication.kt
@@ -1,10 +1,11 @@
 package io.github.drawjustin.kairos
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 // JwtProperties 같은 설정 객체를 @ConfigurationProperties로 자동 등록한다.
 @ConfigurationPropertiesScan
 class KairosApplication
